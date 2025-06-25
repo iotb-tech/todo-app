656b12193a07e458d8587403e7a5bedfaa34fa78
Haleemah <alimotabiona@gmail.com>
2025-06-25 14:31:34 +0100

    landing page of the ToDo app

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("You are welcome to my Todo App!");
+const button = document.getElementById("text-button");
+button.addEventListener("click", function(){
+window.location.href = "login.html";
+})
