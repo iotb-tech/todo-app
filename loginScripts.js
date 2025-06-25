2917e8ba30a3cfb04d3057398b7516fb68190407
Roqeeb <shafiriyuroqeeb@gmail.com>
2025-06-25 18:39:20 +0100

    I removed local storage temporarily

--- loginScripts.js
+++ loginScripts.js
@@ -6,13 +6,4 @@ const loginButton = document.getElementById("login-button");
 loginButton.addEventListener("click", () => {
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("passwordInput").value.trim();
-  //   const storedUsername = localStorage.getItem("username");
-  //   const storedpassword = localStorage.getItem("password");
-
-  //   if (username === storedUsername && password === storedpassword) {
-  //     location.href = "#";
-  //   } else {
-  //     errorMessage.textContent =
-  //       "Invalid username or password, Please try again.";
-  //   }
 });
