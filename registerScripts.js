18dadc86981c833d0dbb1f2a311cd373c81cb65b
Awwal Adewuyi <almajiri.dev@gmail.com>
2025-06-28 15:21:36 +0100

    update redirect after register

--- registerScripts.js
+++ registerScripts.js
@@ -1,18 +1,16 @@
-const registerButton = document.getElementById("register-button");
-const registerDialog = document.getElementById("register-dialog");
-const dialogText = document.getElementById("register-text");
-const closeDialogButton = document.querySelector("#register-dialog button");
-const passwordErrorMssg = document.getElementById("error-message");
+const registerButton = document.getElementById('register-button');
+const registerDialog = document.getElementById('register-dialog');
+const dialogText = document.getElementById('register-text');
+const closeDialogButton = document.querySelector('#register-dialog button');
+const passwordErrorMssg = document.getElementById('error-message');
 
-registerButton.addEventListener("click", () => {
-  const nameInput = document.getElementById("name-input").value.trim();
-  const passwordInput = document.getElementById("password-input").value.trim();
-  const confirmPasswordInput = document
-    .getElementById("confirm-password-input")
-    .value.trim();
+registerButton.addEventListener('click', () => {
+  const nameInput = document.getElementById('name-input').value.trim();
+  const passwordInput = document.getElementById('password-input').value.trim();
+  const confirmPasswordInput = document.getElementById('confirm-password-input').value.trim();
 
   if (!nameInput || !passwordInput || !confirmPasswordInput) {
-    passwordErrorMssg.textContent = "All fields are required!";
+    passwordErrorMssg.textContent = 'All fields are required!';
     return;
   }
 
@@ -22,19 +20,20 @@ registerButton.addEventListener("click", () => {
   }
 
   if (passwordInput !== confirmPasswordInput) {
-    passwordErrorMssg.textContent = "Passwords do not match!";
+    passwordErrorMssg.textContent = 'Passwords do not match!';
     return;
   }
 
-  passwordErrorMssg.textContent = ""; // Clear errors
-  localStorage.setItem("username", nameInput);
-  localStorage.setItem("password", passwordInput);
+  passwordErrorMssg.textContent = ''; // Clear errors
+  localStorage.setItem('username', nameInput);
+  localStorage.setItem('password', passwordInput);
 
   dialogText.innerHTML = `✅ <br/> Welcome, ${nameInput}! Registration successful.`;
-  document.getElementById("input-form").reset();
+  document.getElementById('input-form').reset();
   registerDialog.showModal();
 });
 
-closeDialogButton.addEventListener("click", () => {
+closeDialogButton.addEventListener('click', () => {
   registerDialog.close();
+  window.location.href = 'todo-list.html';
 });
